0b47ddc652766f4a9c592d82be9c624691224287
SirAltitude <74191280+SirAltitude@users.noreply.github.com>
2021-04-23 15:26:35 +0200

    working client server comm

--- Project/src/main/java/Client.java
+++ Project/src/main/java/Client.java
@@ -1,24 +1,23 @@
 import java.io.*;
 import java.net.*;
+class Client {
 
-public class Client {
-    public void sendMsg() throws IOException {
-        String buffer;
-        String modifiedBuffer;
+    public static void main(String[] args) throws Exception
+    {
+        String sentence;
+        String modifiedSentence;
 
-        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-        Socket clientSocket = new Socket(InetAddress.getLocalHost(), 6789);
-
+        BufferedReader inFromUser =
+                new BufferedReader(new InputStreamReader(System.in));
+        Socket clientSocket = new Socket("localhost", 6789);
         DataOutputStream outToServer =
                 new DataOutputStream(clientSocket.getOutputStream());
-
         BufferedReader inFromServer =
                 new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-        buffer = inFromUser.readLine();
-        outToServer.writeBytes(buffer + '\n');
-        modifiedBuffer = inFromServer.readLine();
-        System.out.println("FROM SERVER: " + modifiedBuffer);
+        sentence = inFromUser.readLine();
+        outToServer.writeBytes(sentence + '\n');
+        modifiedSentence = inFromServer.readLine();
+        System.out.println("FROM SERVER: " + modifiedSentence);
         clientSocket.close();
 
     }
--- Project/src/main/java/Server.java
+++ Project/src/main/java/Server.java
@@ -1,28 +1,21 @@
 import java.io.*;
 import java.net.*;
-
-public class Server {
-
-    public static void main(String argv[]) throws Exception
+class Server {
+    public static void main(String[] args) throws Exception
     {
-        String clientBuf;
-        String modifiedBuf;
+        String clientSentence;
+        String capitalizedSentence;
 
         ServerSocket welcomeSocket = new ServerSocket(6789);
-
         while(true) {
             Socket connectionSocket = welcomeSocket.accept();
             BufferedReader inFromClient =
                     new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-
             DataOutputStream  outToClient =
                     new DataOutputStream(connectionSocket.getOutputStream());
-
-            clientBuf = inFromClient.readLine();
-
-            modifiedBuf = clientBuf.toUpperCase() + '\n';
-
-            outToClient.writeBytes(modifiedBuf);
+            clientSentence = inFromClient.readLine();
+            capitalizedSentence = clientSentence.toUpperCase() + '\n';
+            outToClient.writeBytes(capitalizedSentence);
         }
     }
 }
