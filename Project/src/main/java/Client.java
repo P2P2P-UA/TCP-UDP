3cb2c3ccae25d173f143ad785d9aceda59b47a84
SirAltitude <74191280+SirAltitude@users.noreply.github.com>
2021-04-25 00:22:02 +0200

    working file transfer

--- Project/src/main/java/Client.java
+++ Project/src/main/java/Client.java
@@ -2,14 +2,15 @@ import java.io.*;
 import java.net.*;
 class Client {
 
-    public static void main(String[] args) throws Exception
-    {
+    public static void main(String[] args) throws Exception {
         String sentence;
         String modifiedSentence;
 
+        System.out.println("Enter the name of the file you would like to transfer: ");
         BufferedReader inFromUser =
                 new BufferedReader(new InputStreamReader(System.in));
         Socket clientSocket = new Socket("localhost", 6789);
+
         DataOutputStream outToServer =
                 new DataOutputStream(clientSocket.getOutputStream());
         BufferedReader inFromServer =
@@ -18,6 +19,16 @@ class Client {
         outToServer.writeBytes(sentence + '\n');
         modifiedSentence = inFromServer.readLine();
         System.out.println("FROM SERVER: " + modifiedSentence);
+
+        if (modifiedSentence.equals("FILE EXISTS"))
+        {
+            InputStream is = clientSocket.getInputStream();
+            FileOutputStream fr = new FileOutputStream("D:\\SEM6\\Distributed\\FT test\\test.txt");
+            byte size[] = new byte[5000];
+            is.read(size,0,size.length);
+            fr.write(size, 0,size.length);
+        }
+        else System.out.println("Error: no such file found");
         clientSocket.close();
 
     }
--- Project/src/main/java/Server.java
+++ Project/src/main/java/Server.java
@@ -1,21 +1,38 @@
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+
 class Server {
+    private static String fileInput = null;
     public static void main(String[] args) throws Exception
     {
         String clientSentence;
         String capitalizedSentence;
+        ArrayList<String> availFiles = new ArrayList<>();
+        availFiles.add("FTP.txt");
+
+        FileInputStream file = new FileInputStream("D:\\Documents\\FTP.txt");
 
         ServerSocket welcomeSocket = new ServerSocket(6789);
-        while(true) {
-            Socket connectionSocket = welcomeSocket.accept();
-            BufferedReader inFromClient =
-                    new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-            DataOutputStream  outToClient =
-                    new DataOutputStream(connectionSocket.getOutputStream());
-            clientSentence = inFromClient.readLine();
-            capitalizedSentence = clientSentence.toUpperCase() + '\n';
-            outToClient.writeBytes(capitalizedSentence);
+        Socket connectionSocket = welcomeSocket.accept();
+
+        BufferedReader inFromClient =
+                new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+        DataOutputStream  outToClient =
+                new DataOutputStream(connectionSocket.getOutputStream());
+        clientSentence = inFromClient.readLine();
+
+        if(clientSentence.equals(availFiles.get(0))) {
+            outToClient.writeBytes("FILE EXISTS\n");
+            byte size[] = new byte[5000];
+            file.read(size,0,size.length); // read file from start to end
+            OutputStream os = connectionSocket.getOutputStream();
+            os.write(size,0,size.length);
+
+
+//            fileInput = "C:\\Users\\malon\\Desktop\\" + clientSentence;
+//            capitalizedSentence = clientSentence.toUpperCase() + '\n';
+//            outToClient.writeBytes(capitalizedSentence);
         }
     }
 }
